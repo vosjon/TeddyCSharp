74ed2d7e0ed97f1cbf072bb16ac0a1b23e87542e
Jonathan Vos <vosjonjoe@gmail.com>
2025-01-06 10:16:30 -0800

    Completed video 2.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("This is a program.");
+
+
+// Video 2: Variables
+
+string billGates = "Praise the great leader... BILL GATES";
+Console.WriteLine(billGates);
+
+int numberOfBillGates = 1;
+Console.WriteLine(numberOfBillGates);
+
+var name = "Jon";
+var age = 37;
+Console.WriteLine($"My name is {name} and my age is {age}.");
+
+// Video 3: Data Types
+
